65b15f6d2d054fce0bf23d746f373e5015e9ad9d
Kyle <kyle.fetterhoff@gmail.com>
2020-02-29 10:05:53 -0500

    Add ability to grow dots

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const container = document.getElementById("container");
-const dotArr = [];
+const dotsArr = [];
 const rows = 15;
 const cols = 15;
 
 // Create dots
 for (let i = 0; i < rows; i++) {
+  dotsArr[i] = [];
   for (let j = 0; j < cols; j++) {
     const dot = document.createElement("div");
     dot.classList.add("dot");
     container.appendChild(dot);
+    dotsArr[i].push(dot);
+  }
+}
+
+dotsArr.forEach((cols, i) => {
+  cols.forEach((dot, j) => {
+    dot.addEventListener("click", () => {
+      growDot(i, j);
+    });
+  });
+});
+
+function growDot(i, j) {
+  if (!dotsArr[i][j].classList.contains("grow")) {
+    dotsArr[i][j].classList.add("grow");
   }
 }
